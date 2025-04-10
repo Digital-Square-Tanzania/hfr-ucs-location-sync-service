319aeae95889324668b435d3cafa3b3c6d6ee8f8
cozej4 <ilakozejumanne@gmail.com>
2025-04-10 16:03:47 +0300

    :hammer: Added unit tests

--- src/test/java/tz/go/moh/ucs/util/HttpUtilTest.java
+++ src/test/java/tz/go/moh/ucs/util/HttpUtilTest.java
@@ -1,14 +1,21 @@
 package tz.go.moh.ucs.util;
 
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 import org.apache.http.client.methods.HttpRequestBase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import tz.go.moh.ucs.domain.HttpMethod;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class HttpUtilTest {
+
+    private MockWebServer mockWebServer;
 
     @Test
     void testRemoveEndingSlash_RemovesSlash() {
@@ -78,5 +85,56 @@ class HttpUtilTest {
     void testMakeConnection_DELETE() throws URISyntaxException {
         HttpRequestBase request = HttpUtil.makeConnection("http://example.com/api", null, HttpMethod.DELETE, HttpUtil.AuthType.NONE, "");
         assertEquals("DELETE", request.getMethod());
+    }
+
+    @BeforeEach
+    void setUp() throws IOException {
+        mockWebServer = new MockWebServer();
+        mockWebServer.start();
+    }
+
+    @AfterEach
+    void tearDown() throws IOException {
+        mockWebServer.shutdown();
+    }
+
+    @Test
+    void testGetURL_withValidResponse_shouldReturnResponseBody() throws IOException {
+        String expectedResponse = "{\"message\":\"success\"}";
+        mockWebServer.enqueue(new MockResponse()
+                .setBody(expectedResponse)
+                .setResponseCode(200));
+
+        String baseUrl = mockWebServer.url("/test").toString();
+        String response = HttpUtil.getURL(baseUrl, "user", "pass");
+
+        assertNotNull(response);
+        assertEquals(expectedResponse, response);
+    }
+
+    @Test
+    void testGetURL_withEmptyResponse_shouldReturnNull() throws IOException {
+        mockWebServer.enqueue(new MockResponse()
+                .setBody("")
+                .setResponseCode(200));
+
+        String baseUrl = mockWebServer.url("/empty").toString();
+        String response = HttpUtil.getURL(baseUrl, "user", "pass");
+
+        assertNull(response);
+    }
+
+    @Test
+    void testGetURL_withErrorResponse_shouldReturnErrorBody() throws IOException {
+        String errorResponse = "{\"error\":\"something went wrong\"}";
+        mockWebServer.enqueue(new MockResponse()
+                .setBody(errorResponse)
+                .setResponseCode(500));
+
+        String baseUrl = mockWebServer.url("/error").toString();
+        String response = HttpUtil.getURL(baseUrl, "user", "pass");
+
+        assertNotNull(response);
+        assertEquals(errorResponse, response);
     }
 }
